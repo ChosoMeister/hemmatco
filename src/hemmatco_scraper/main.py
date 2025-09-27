e11bc68dada71564758d1578298aeea2580d39bf
Mustafa Tayefi <mustafa.tayefi@gmail.com>
2025-09-27 13:31:33 +0100

    Fix scraper to pull posts via API and log deliveries

--- src/hemmatco_scraper/main.py
+++ src/hemmatco_scraper/main.py
@@ -29,8 +29,10 @@ def dispatch_posts(settings: Settings, posts: list[Post], state: State) -> None:
 
     initial_run = not state.processed_urls()
     total = len(posts)
+    logger.info("Dispatching %s post(s) to Telegram", total)
 
     for idx, post in enumerate(posts):
+        logger.info("Processing post %s/%s: %s", idx + 1, total, post.url)
         if not post.image_urls:
             logger.info("Skipping %s because no images were found", post.url)
         else:
@@ -40,8 +42,10 @@ def dispatch_posts(settings: Settings, posts: list[Post], state: State) -> None:
                 settings.telegram_topic_id,
                 format_post(post),
             )
+            logger.info("Sent %s with %s image link(s)", post.url, len(post.image_urls))
         state.mark_processed([post.url])
         state.save()
+        logger.info("Recorded %s as processed", post.url)
         if idx < total - 1:
             sleep_between_posts(idx, total, settings, initial_run)
 
